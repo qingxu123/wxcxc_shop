29fe912b446e944b03ba81885a1e739c7c99c3b9
unknown <2537937571@qq.com>
2020-11-08 18:22:03 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 import Vue from 'vue'
 import App from './App'
 
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+//跟路径
+$http.baseUrl='https://www.uinav.com'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+// 请求拦截器
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+
+// 响应拦截器
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+//封装弹框的方法
+uni.$showMsg = function(title = '数据请求失败!', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
