59f291fb111e7588a1f32aafe4a4ea7db65d1bcb
unknown <2537937571@qq.com>
2020-11-12 19:10:44 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		goods_list
+		<view class="goods_list">
+		  <view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)"> 
+        <my-goods :goods="goods"></my-goods>
+      </view>
+		</view>
 	</view>
 </template>
 
@@ -8,9 +12,53 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj:{
+          query:'',
+          cid:'',
+          pagenum:1,
+          pagesize:10
+        },
+        goodsList:[],
+        total:0,
+        isLoading:false
 			};
-		}
+		},
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods:{
+      async getGoodsList(cb){
+        this.isLoading =true
+        const {data:res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+         this.isLoading =false
+         cb && cb()
+        if(res.meta.status !==200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+      },
+      gotoDetail(goods){
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+        })
+      }
+    },
+    onReachBottom(){
+      if(this.queryObj.pagenum*this.queryObj.pagesize >=this.total) return uni.$showMsg('数据加载完毕')
+      if(this.isLoading) return
+      this.queryObj.pagenum +=1
+      this.getGoodsList()
+    },
+    onPullDownRefresh(){
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isLoading = false
+      this.goodsList=[]
+      
+      this.getGoodsList(()=> uni.stopPullDownRefresh())
+    }
+   
 	}
 </script>
 
